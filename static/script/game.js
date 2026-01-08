420dc97561a4ea16c76b7488b8335aafd06ace62
Lawson Han <lawsonhan123@gmail.com>
2026-01-08 12:09:23 +0800

    Remove deprecated meta tags from index.html and clean up requestAnimationFrame polyfill in game.js for improved performance and modern standards.

--- static/script/game.js
+++ static/script/game.js
@@ -3,29 +3,6 @@
 * Mini game engine
 */
 
-// requestAnimationFrame polyfill
-if (!Date.now)
-Date.now = function() { return new Date().getTime(); };
-(function() {
-    'use strict';
-    const vendors = ['webkit', 'moz'];
-    for (let i = 0; i < vendors.length && !window.requestAnimationFrame; ++i) {
-        const vp = vendors[i];
-        window.requestAnimationFrame = window[vp+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = (window[vp+'CancelAnimationFrame'] || window[vp+'CancelRequestAnimationFrame']);
-    }
-    if (/iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) // iOS6 is buggy
-    || !window.requestAnimationFrame || !window.cancelAnimationFrame) {
-        let lastTime = 0;
-        window.requestAnimationFrame = function(callback) {
-            const now = Date.now();
-            const nextTime = Math.max(lastTime + 16, now);
-            return setTimeout(function() { callback(lastTime = nextTime); },
-            nextTime - now);
-        };
-        window.cancelAnimationFrame = clearTimeout;
-    }
-}());
 
 function Game(id,params){
     const _ = this;
